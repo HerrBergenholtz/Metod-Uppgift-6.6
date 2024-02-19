8ade2d4daf97d399edca8179ac5da1e5956171fc
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-19 22:07:41 +0100

    Add project files.

---
+++ Metod-Uppgift-6.6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift_6_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Skriv in en sträng");
+            Caps(Console.ReadLine());
+        }
+        static void Caps(string sträng)
+        {
+            string[] str = sträng.Split(' ');
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0) str[i] = str[i].ToUpper();
+            }
+
+            sträng = string.Join(" ", str);
+
+            Console.WriteLine(sträng);
+        }
+    }
+}
